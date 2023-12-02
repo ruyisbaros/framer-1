40550ce8df11acadb2062bdb3679e3fdf6659d64
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-12-02 19:46:30 +0100

    project changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import AnimateOne from "./pages/AnimateOne";
+import Header from "./components/lessonOne/Header";
+import Home from "./components/lessonOne/Home";
+import Base from "./components/lessonOne/Base";
+import Toppings from "./components/lessonOne/Toppings";
+import Order from "./components/lessonOne/Order";
 
-const App = () => {
+function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base });
+  };
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter((item) => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  };
+
   return (
-    <div className="App">
+    <>
+      <Header />
       <Routes>
-        <Route path="/" element={<AnimateOne />} />
+        <Route
+          path="/base"
+          element={<Base addBase={addBase} pizza={pizza} />}
+        />
+        <Route
+          path="/toppings"
+          element={<Toppings addTopping={addTopping} pizza={pizza} />}
+        />
+        <Route path="/order" element={<Order pizza={pizza} />} />
+        <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </>
   );
-};
+}
 
 export default App;
---
+++ src/pages/FramerOne.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FramerOne = () => {
+  return <div>FramerOne</div>;
+};
+
+export default FramerOne;
